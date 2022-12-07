592903865319e544f6859e1a4fa5e2831e1ae769
Shelton Han <shelton.han@metigy.com>
2022-12-08 07:38:35 +1100

    Initial commit

---
+++ app/src/main/java/com/sh/kotlin/dogs/view/ListFragment.kt
@@ -0,0 +1,31 @@
+package com.sh.kotlin.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sh.kotlin.dogs.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/sh/kotlin/dogs/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sh.kotlin.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sh.kotlin.dogs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
