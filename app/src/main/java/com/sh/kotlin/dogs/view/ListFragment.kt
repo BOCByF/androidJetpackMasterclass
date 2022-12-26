147bde90cb3ae6349ddda824604ff262c1ff2fa1
BOCByF <hanxt90@gmail.com>
2022-12-27 01:48:09 +1100

    Add navigation
    Add navigation

--- app/src/main/java/com/sh/kotlin/dogs/view/ListFragment.kt
+++ app/src/main/java/com/sh/kotlin/dogs/view/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.sh.kotlin.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -25,6 +27,15 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetials.setOnClickListener {
+            val navDir = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(navDir)
+        }
     }
 
 
--- app/src/main/java/com/sh/kotlin/dogs/view/MainActivity.kt
+++ app/src/main/java/com/sh/kotlin/dogs/view/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.sh.kotlin.dogs.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.sh.kotlin.dogs.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+
 }
