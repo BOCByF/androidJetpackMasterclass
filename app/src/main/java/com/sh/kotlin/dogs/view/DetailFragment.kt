bc930a75390c9daca3a2a26b969bcf49675e03fe
BOCByF <hanxt90@gmail.com>
2023-03-31 09:51:32 +1100

    Add Palette

--- app/src/main/java/com/sh/kotlin/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/sh/kotlin/dogs/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.sh.kotlin.dogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.sh.kotlin.dogs.R
 import com.sh.kotlin.dogs.databinding.FragmentDetailBinding
 import com.sh.kotlin.dogs.model.util.getProgressDrawable
@@ -55,8 +61,33 @@ class DetailFragment : Fragment() {
 
     fun observeViewModel() {
         viewModel.dog.observe(this, Observer {
-               dataBinding.dog = it
+            dataBinding.dog = it
+            it.imageUrl?.let {
+                setupBackgroundColor(it)
+            }
         })
     }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.dominantSwatch?.rgb ?: 0
+                            val myPalette = DogPalette(intColor)
+                            dataBinding.palette = myPalette
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+            })
+    }
+
 }
 
+data class DogPalette(var color: Int)
